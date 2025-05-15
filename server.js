888c7aad8dbdc1887eb5007ed53beb06c5f12c8f
Chsathwik-641 <chepyalasathwik641@gmail.com>
2025-05-15 17:11:31 +0530

    made a change

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/clients", clientRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/team", teamRoutes);
-app.use("/api/invoices", invoiceRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/users", userRoutes);
+// app.use("/api/clients", clientRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/team", teamRoutes);
+// app.use("/api/invoices", invoiceRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/users", userRoutes);
 
 const errorHandler = require("./middleware/error");
 app.use(errorHandler);
