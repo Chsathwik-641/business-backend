ce3439a46fa57d2f653b27280c8a894f9e89abf8
Chsathwik-641 <chepyalasathwik641@gmail.com>
2025-05-15 16:07:02 +0530

    made change in cors

--- server.js
+++ server.js
@@ -21,11 +21,8 @@ app.use(
     origin:
       "https://business-frontend-git-main-chsathwik-641s-projects.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 app.use(express.json());
 app.use(morgan("dev"));
 
